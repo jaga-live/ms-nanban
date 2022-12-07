74df836c010f9e26b82ca916b1a0ce4d98cf07bb
Jaga <jagadheesh6@gmail.com>
2022-12-07 16:18:45 +0530

    fix - auth middleware

--- src/api/auth/middleware/auth.middleware.ts
+++ src/api/auth/middleware/auth.middleware.ts
@@ -22,20 +22,20 @@ export class AuthGuard extends BaseMiddleware {
 
 			const jwt = auth.split(' ')[1];
 			const decoded: any = verify(jwt, this.JWT_SECRET);
-
+			
 			/// //Fetch User data
 			const user = await this.userRepo.find_by_id(decoded.id);
 			if (!user) throw new HttpException('Unauthorized Exception', 401);
-
+			
 			/// //validate User session form DB
-			const authSession = await this.authRepo.get_auth_session_with_id(decoded.id, decoded.sessionId);
+			const authSession = await this.authRepo.get_auth_session_with_id(decoded.userId, decoded.sessionId);
 			if (!authSession) throw new HttpException('Unauthorized Exception', 401);
 
 			req.userData = { ...decoded };
 			req.authToken = jwt;
 			next();
 		} catch (error) {
-			console.log(error);
+			// console.log(error);
 			return res.status(401).json({
 				error: 'Unauthorized Exception',
 			});
